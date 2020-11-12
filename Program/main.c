f6c25ec7b36585826846753be0c4d21f3b1d50c4
Robbetest123 <robbe.Croes@student.vives.be>
2020-11-12 18:30:56 +0100

    third commit

--- Program/main.c
+++ Program/main.c
@@ -4,75 +4,41 @@
 #include <string.h>
 #include <mysql/mysql.h>
 
-// void finish_with_error(MYSQL *conn){
-//         fprintf(stderr, "%s\n",mysql_error(conn));
-//         mysql_close(conn);
-//         exit(1);
-//     }
+void finish_with_error(MYSQL *conn){
+        fprintf(stderr, "%s\n",mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+    }
 
-// int main(int args, char **argv)
-// {
-// 	MYSQL *conn = mysql_init(NULL);
-	
-// 	if(conn == NULL)
-// 	{
-// 		fprintf(stderr, "%s\n",mysql_error(conn));
-//         mysql_close(conn);
-//         exit(1);
-// 	}
-
-// 	if(mysql_real_connect(conn,"Localhost","pi","raspberry","Lab_embeded2_1",0,NULL,0) == NULL)
-// 	{
-// 		fprintf(stderr, "%s\n",mysql_error(conn));
-//         mysql_close(conn);
-//         exit(1);
-// 	}
-
-// 	int status =1;
-// 	char operation[56];
-
-// 	INP_GPIO(27);
-
-// 	while(1){
-
-// 		if(GPIO_READ(27) == 1){
-// 			printf("de pin is hoog");
-// 		}
-// 		else{
-// 			printf("de pin is laag");
-// 		}
-// 		scanf("fdkjsgb");
-		
-// 		// sprintf(operation,"INSERT INTO GPIO_Uitlezen VALUES(NULL,%d,%d,NULL)",status); // versturen naar db
-// 		// if (mysql_query(conn,operation)) {
-// 		// finish_with_error(conn);
-// 		// }
-// 	}
-	
-
-// 	mysql_close(conn);
-//   	exit(0);
-// }
-
-
-int main()
+int main(int args, char **argv)
 {
 	if(map_peripheral(&gpio) == -1) 
 	{
        	 	printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
         	return -1;
     	}
+		MYSQL *conn = mysql_init(NULL);
+	
+	if(conn == NULL)
+	{
+		fprintf(stderr, "%s\n",mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+	}
 
-	// Define gpio 17 as output
-	// INP_GPIO(17);
-	// OUT_GPIO(17);
+	if(mysql_real_connect(conn,"Localhost","pi","raspberry","Lab_embeded2_1",0,NULL,0) == NULL)
+	{
+		fprintf(stderr, "%s\n",mysql_error(conn));
+        mysql_close(conn);
+        exit(1);
+	}
+	char operation[50];
+	int pin;
 	int oudestatus = 0;
 	int nieuwestatus = 0;
-
 	INP_GPIO(26);
 	while(1)
 	{
-		
 		if(GPIO_READ(26))
 		{
 			nieuwestatus = 1;
@@ -81,21 +47,26 @@ int main()
 		{
 			nieuwestatus = 0;
 		}
-		
 		if(oudestatus != nieuwestatus)
 		{
 			if(GPIO_READ(26)){
 			printf("de pin is hoog\n");
+			pin = 26;
+			sprintf(operation,"INSERT INTO GPIO_Uitlezen VALUES(NULL,'%d','%d',NULL)",pin, nieuwestatus); // versturen naar db
+			if (mysql_query(conn,operation)) {
+			finish_with_error(conn);
+			}
 			}
 			else{
 			printf("de pin is laag\n");
+			pin = 26;
+			sprintf(operation,"INSERT INTO GPIO_Uitlezen VALUES(NULL,'%d','%d',NULL)",pin, nieuwestatus); // versturen naar db
+			if (mysql_query(conn,operation)) {
+			finish_with_error(conn);
+			}
 			}
 			oudestatus = nieuwestatus;
 		}
-		
-		
-			
-
 	}
 
 	return 0;	
